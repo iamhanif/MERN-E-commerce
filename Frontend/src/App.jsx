dd793747acd19e5ea582130f9a04308e3496c3bc
Hanif <hanif@Hanifs-MacBook-Air.local>
2024-10-11 12:42:58 +0600

    navbar done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { Button } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Create from "./pages/Create";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Button> hello </Button>
-    </>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </Box>
   );
 }
 
